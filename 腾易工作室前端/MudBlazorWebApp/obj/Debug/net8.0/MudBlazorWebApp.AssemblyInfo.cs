b44917ad85ca79e8173d53ab5e7d7dc11e29faad
PC-20240510DNXV\Administrator <2216528769@qq.com>
2024-12-02 12:32:36 +0800

    Test

--- 腾易工作室前端/MudBlazorWebApp/obj/Debug/net8.0/MudBlazorWebApp.AssemblyInfo.cs
+++ 腾易工作室前端/MudBlazorWebApp/obj/Debug/net8.0/MudBlazorWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MudBlazorWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+657aa157bad6309b6c3ba093b3213c74cffc809e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07add6e5e658b92250865fd08b4c8ed35fba3f73")]
 [assembly: System.Reflection.AssemblyProductAttribute("MudBlazorWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MudBlazorWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
